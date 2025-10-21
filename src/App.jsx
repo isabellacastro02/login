3e0a98b0f1e4d91887db5ec7f7b7cdba6c5feac5
isabellacastro02 <isabella.mcastro02@gmail.com>
2025-10-21 08:06:36 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <header>
+      <h1>LOGIN</h1>
+      <p LOGAR COM></p>
+      </header>
+      <input 
+            type="text"
+            placeholder="Digite o nome do usuário.."
+            />
+            <button>ENTRAR</button>
+      
+    </>
+  )
+}
+
+export default App
